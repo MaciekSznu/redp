125ef9dc0415cfdbad98bdc6b6ef08e83b9e3d5b
Maciej Sznurawa <msznurawa@gmail.com>
2020-11-23 10:50:06 +0100

    update browserslist, fix unused imports bug

--- src/components/Header/TopMenuItemsSecondary.js
+++ src/components/Header/TopMenuItemsSecondary.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import styles from "./TopMenuItemsSecondary.module.scss";
+// import { Link } from "react-router-dom";
+// import styles from "./TopMenuItemsSecondary.module.scss";
 
 class TopMenuItemsSecondary extends React.Component {
   render() {
