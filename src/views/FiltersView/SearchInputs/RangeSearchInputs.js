a0dcc7b227cfed64a0dd278eb0c7c5ebaec78b8d
Maciej Sznurawa <msznurawa@gmail.com>
2020-12-07 14:56:31 +0100

    Add rangesliders dynamic rendering values

--- src/views/FiltersView/SearchInputs/RangeSearchInputs.js
+++ src/views/FiltersView/SearchInputs/RangeSearchInputs.js
@@ -5,16 +5,24 @@ import RoomsRangeContainer from "./RoomsRangeInput";
 import AreaRangeContainer from "./AreaRangeInput";
 import PriceRangeContainer from "./PriceRangeInput";
 class RangeSearchInputs extends React.Component {
-
   render(props) {
     const filters = this.props.filters;
+    const formatPrice = (price) => {
+      if (price !== undefined) {
+        return price.toLocaleString();
+      }
+    };
     return (
       <div className={styles.FiltersWrapper}>
         <div className={styles.FilterFloors}>
           <h3 className={styles.FilterTitle}>Piętro</h3>
           <div className={styles.FilterValuesWrapper}>
-            <span className={styles.FilterValue}>od {filters.selectedFloors.minValue || filters.selectedFloors.min || '0'}</span>
-            <span className={styles.FilterValue}>do {filters.selectedFloors.maxValue || filters.selectedFloors.max}</span>
+            <span className={styles.FilterValue}>
+              od {filters.selectedFloors.minValue || filters.selectedFloors.min || "0"}
+            </span>
+            <span className={styles.FilterValue}>
+              do {filters.selectedFloors.maxValue || filters.selectedFloors.max}
+            </span>
           </div>
           <FloorRangeContainer onFloorChange={this.props.onFloorChange} />
         </div>
@@ -29,16 +37,24 @@ class RangeSearchInputs extends React.Component {
         <div className={styles.FilterArea}>
           <h3 className={styles.FilterTitle}>Metraż</h3>
           <div className={styles.FilterValuesWrapper}>
-            <span className={styles.FilterValue}>od {filters.selectedArea.minValue || filters.selectedArea.min} m<sup>2</sup></span>
-            <span className={styles.FilterValue}>do {filters.selectedArea.maxValue || filters.selectedArea.max} m<sup>2</sup></span>
+            <span className={styles.FilterValue}>
+              od {filters.selectedArea.minValue || filters.selectedArea.min} m<sup>2</sup>
+            </span>
+            <span className={styles.FilterValue}>
+              do {filters.selectedArea.maxValue || filters.selectedArea.max} m<sup>2</sup>
+            </span>
           </div>
           <AreaRangeContainer onAreaChange={this.props.onAreaChange} />
         </div>
         <div className={styles.FilterPrice}>
           <h3 className={styles.FilterTitle}>Cena</h3>
           <div className={styles.FilterValuesWrapper}>
-            <span className={styles.FilterValue}>od {filters.selectedPrice.minValue || filters.selectedPrice.min} zł</span>
-            <span className={styles.FilterValue}>do {filters.selectedPrice.maxValue || filters.selectedPrice.max} zł</span>
+            <span className={styles.FilterValue}>
+              od {formatPrice(filters.selectedPrice.minValue) || formatPrice(filters.selectedPrice.min)} zł
+            </span>
+            <span className={styles.FilterValue}>
+              do {formatPrice(filters.selectedPrice.maxValue) || formatPrice(filters.selectedPrice.max)} zł
+            </span>
           </div>
           <PriceRangeContainer onPriceChange={this.props.onPriceChange} />
         </div>
--- src/views/FiltersView/SearchInputs/RoomsRangeInput.js
+++ src/views/FiltersView/SearchInputs/RoomsRangeInput.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { roomsRange } from '../../../components/Ranges/RangesData';
-import InputRange from 'react-input-range';
-import './RangeSearchInputs.scss';
+import React from "react";
+import { roomsRange } from "../../../components/Ranges/RangesData";
+import InputRange from "react-input-range";
+import "./RangeSearchInputs.scss";
 
 class RoomsRangeContainer extends React.Component {
   constructor(props) {
@@ -12,7 +12,17 @@ class RoomsRangeContainer extends React.Component {
 
   render() {
     return (
-      <InputRange minValue={this.state.minValue} maxValue={this.state.maxValue} value={this.state.value} step={this.state.step} onChange={value => this.setState({value})} onChangeComplete={value => this.props.onRoomsChange({rooms: value})} allowSameValues={true} />
+      <InputRange
+        minValue={this.state.minValue}
+        maxValue={this.state.maxValue}
+        value={this.state.value}
+        step={this.state.step}
+        onChange={(value) => {
+          this.setState({ value });
+          this.props.onRoomsChange({ rooms: value });
+        }}
+        allowSameValues={true}
+      />
     );
   }
 }
